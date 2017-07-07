0672b34bf73f6dcfff55c3117969a913ec9e5b6e
JK <james.kirk@warwick.ac.uk>
2017-07-07 15:56:34 +0100

    Added EFOSC to column checker

--- interactive_column_check.py
+++ interactive_column_check.py
@@ -12,6 +12,7 @@ parser.add_argument("-y","--y_collapse",help="if wanting to collapse in y rather
 parser.add_argument("-px","--pixel",help="use this to input x pixel position, overriding use of mouse click",type=float)
 parser.add_argument("-s","--slit_width",help='use to define which slit is used, default = 40"',type=int,default=40)
 parser.add_argument("-w","--window",help='use to overplot window in red, default = 1 2148 1 2500"',nargs='+',default=[1,2148,1,2500])
+parser.add_argument("-inst","--instrument",help="Which instrument? Needed for field of view & slit width and length. Default = ACAM",default='ACAM')
 args = parser.parse_args()
 
 f = fits.open(args.fits_file)
@@ -19,6 +20,16 @@ if len(f) == 2:
 	data = f[1].data
 else:
 	data = f[0].data
+	
+if args.instrument == 'EFOSC':
+	arcsec_per_pix = 0.12 # arcsec/pixel in manual
+	pix_conversion = 2048/1030. # A 2048x2048 produces a 1030x1030 array?
+	pix_scale = pix_conversion*arcsec_per_pix
+	y_range = [1030/2-7.5/pix_scale,1030/2+7.5/pix_scale]
+	x_range = [0,1030] 
+	
+if args.instrument == 'ACAM':
+	pix_scale = 0.25 # arcsec/pixel
 
 colours = ['r','g','b','k','c','m']
 
@@ -41,10 +52,10 @@ def plot_slit(fits_file,slit_x_range=[279,1752],slit_y_range=[1247+800,1132+800]
 	
 	return window, x_range,y_range
 
-if args.slit_width == 27:
+if args.slit_width == 27 and args.instrument == 'ACAM':
 	window,x_range,y_range = plot_slit(f)
 	
-else: # Using a 40" slit, which we don't have empirical data for so I'm assuming this is well centred
+elif args.slit_width == 40 and args.instrument == 'ACAM': # Using a 40" slit, which we don't have empirical data for so I'm assuming this is well centred
 	x_centre = np.shape(data)[1]/2.
 	y_centre = np.shape(data)[0]/2.
 	hdr = f[0].header
@@ -73,22 +84,30 @@ else: # Using a 40" slit, which we don't have empirical data for so I'm assuming
 	#~ x_win = [180,1900]
 	#~ y_win = [300,2301]
 	
-x_win = [args.window[0],args.window[1]]
-y_win = [str(int(args.window[2])-800),str(int(args.window[3])-800)]
+else:
+	pass
+	
+if args.instrument == 'ACAM':
+    x_win = [args.window[0],args.window[1]]
+    y_win = [str(int(args.window[2])-800),str(int(args.window[3])-800)]
+if args.instrument == 'EFOSC':
+	x_win = [0,1030]
+	y_win = [0,1030]
 	
 
 
 # ADD FIXED WIDTH BETWEEN STARS, PERHAPS CONVERTING WCS TO PIXEL COORDS? USE X PIXEL BOX OF 100 PIXELS FOR BACKGROUND. 2 strips of 50 pixel width. 2 vertical strips of correct separtion, collapse in x and take ratio of these
 # Choose windows. Which slit?
 
-# ACAM pixel scale
-px_scale = 0.25 # arcsec/pixel
 
 plt.ion()
 
 # PLOT CHIP / FITS FILE
 fig1 = plt.figure(figsize=(16,14))
-plt.imshow(np.log10(data),cmap='gray')
+if args.instrument == 'ACAM':
+    plt.imshow(np.log10(data),cmap='gray')
+if args.instrument == 'EFOSC':
+    plt.imshow(data,cmap='gray')
 plt.xlim(0,np.shape(data)[1])
 plt.ylim(0,np.shape(data)[0])
 
@@ -107,7 +126,7 @@ plt.plot([x_win[0],x_win[0]],y_win,color='r',lw=3)
 plt.plot([x_win[1],x_win[1]],y_win,color='r',lw=3)
 
 if args.pixel != None:
-	x = np.array([[args.pixel,np.mean(y_range)]])
+	x = np.array([[args.pixel,np.mean(y_range)]],dtype=int)
 	args.nclicks = 1
 
 else:
@@ -116,7 +135,7 @@ else:
 
 # Find the position of the comparison relative to the first click
 if args.star_distance != None:
-	x_comp = [x[i][0] + args.star_distance*60/px_scale for i in range(args.nclicks)]
+	x_comp = [x[i][0] + args.star_distance*60/pix_scale for i in range(args.nclicks)]
 	print "x position of comparison = ",x_comp
 
 
@@ -160,7 +179,7 @@ for i in range(args.nclicks):
 		plt.plot(spectra,color=colours[i])
 		
 		if args.star_distance != None:
-			spectra_comp = data[:,x_comp[i]-args.pixel_width:x_comp[i]+args.pixel_width]
+			spectra_comp = data[:,int(x_comp[i]-args.pixel_width):int(x_comp[i]+args.pixel_width)]
 			spec_ratio.append(spectra.sum(axis=1)/np.array(spectra_comp,dtype=float).sum(axis=1))
 			#plt.plot(spectra_comp.sum(axis=1),color=colours[i],ls='--')
 			plt.plot(spectra_comp,color=colours[i+1])
